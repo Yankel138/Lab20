f2aaa8357627a86976ae6e76921094311e5aaeba
medzakovskiy.vi@yandex.ru <medzakovskiy.vi@yandex.ru>
2021-10-28 09:09:41 +0300

    Добавьте файлы проекта.

---
+++ Lab20/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab20
+{
+    class Program
+    {
+        delegate double MyDelegate(double rad);
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус: ");
+            double rad = Convert.ToDouble(Console.ReadLine());
+            MyDelegate myDelegate = new MyDelegate(Length);
+            Console.WriteLine($"Длина окружности с радиусом {rad} = {myDelegate?.Invoke(rad):f2}");
+            myDelegate += Square;
+            Console.WriteLine($"Площадь круга с радиусом {rad} = {myDelegate?.Invoke(rad):f2}");
+            myDelegate += Volume;
+            Console.WriteLine($"Объем шара с радиусом {rad} = {myDelegate?.Invoke(rad):f2}");
+
+            Console.ReadKey();
+        }
+
+
+
+        static double Length(double rad)
+        {
+            return 2 * Math.PI * rad;
+        }
+
+        static double Square(double rad)
+        {
+            return Math.PI * rad * rad;
+        }
+
+        static double Volume(double rad)
+        {
+            return 4 / 3 * Math.PI * Math.Pow(rad, 3);
+        }
+    }
+}
